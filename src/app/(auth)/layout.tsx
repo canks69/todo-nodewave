0229dc79bbe6b9dc2f5bd2a5a3852d73371eeae6
canks <canks.official@gmail.com>
2025-07-25 10:08:39 +0800

    Enable vertical scrolling for auth layout's main container

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -4,7 +4,7 @@ export default function AuthLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <main className="flex-grow flex flex-col items-center pt-24 px-4 sm:px-6 lg:px-8 z-20 relative">
+    <main className="flex-grow flex flex-col items-center pt-24 px-4 sm:px-6 lg:px-8 z-20 overflow-y-scroll">
       {children}
     </main>
   );
